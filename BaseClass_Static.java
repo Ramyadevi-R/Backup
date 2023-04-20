11e7695f2effdaeb040f26d8396fb4da4817ba82
Ramyadevi-R <ramyadeviraja003@gmail.com>
2023-04-20 19:34:01 +0530

    added new java files

---
+++ BaseClass_Static.java
@@ -0,0 +1,48 @@
+package Leaftaps;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass_Static {
+
+	public RemoteWebDriver driver;
+	
+	
+	@Parameters({"browser", "url","username","password"})
+	@BeforeMethod
+	public void PreConditions(String browser, String url, String username, String password) {
+
+		if(browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys(username); 
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+	@AfterMethod
+	public void Postconditions() {
+		driver.close();
+	}
+	
+
+}
+
